134ea42a6bafbabc086d22ddcaa37d37c4ea49b4
Devon Acree-Meza <dacreeme@asu.edu>
2021-04-22 19:44:48 -0700

    create write to file function

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var customers = JSON.parse(fs.readFileSync(cF));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { data: { customerData: customers } });
+  res.render("index", { data: {} });
 });
 
 /* GET customer data. */
@@ -16,22 +16,37 @@ router.get("/customerData", function (req, res, next) {
 
 /* POST an edited customer. */
 router.post("/edit", function (req, res, next) {
-  console.log("hello from the muh fucking post");
   // res.send(JSON.stringify(customers));
-  console.log(req.body);
-  let edited = JSON.parse(req.body);
+  // console.log(JSON.parse(req.body));
+
+  //had to get this hacky becuase req.body was giving
+  // [Object: null prototype] {
+  //   '{"firstName":"Leah","lastName":" Skywalker","poolSize":"123","filterType":"Sand"}': ''
+  // }
+  let edited = JSON.parse(Object.keys(req.body)[0]);
+
   writeCustomers(edited);
   //send updated customers list
   res.send(JSON.stringify(customers));
 });
 
 function writeCustomers(newObj) {
+  newObj.id = parseFloat(newObj.id);
+  newObj.poolSize = parseFloat(newObj.poolSize);
   customers = JSON.parse(fs.readFileSync(cF));
-  customers.push(newObj);
-  console.log(newObj);
-  console.log(customers);
+  let found = false;
+  //check if customer already exists
+  customers.forEach((e) => {
+    if (e.id == newObj) {
+      e = newObj;
+      found = true;
+    }
+  });
+  //if the customer wasn't found, then add.
+  if (!found) {
+    customers.push(newObj);
+  }
   fs.writeFileSync(cF, JSON.stringify(customers));
-  console.log("wrote");
 }
 
 module.exports = router;
